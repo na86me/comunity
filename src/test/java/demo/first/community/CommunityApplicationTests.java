e8782da96f112c1be138f4de10a655a5b2a116a9
na86me <15802101988@qq.com>
2020-04-02 21:56:38 +0800

    first commit

---
+++ src/test/java/demo/first/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.first.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
